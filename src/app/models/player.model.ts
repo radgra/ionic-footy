3d2bbe8adfb0ae629684c866b5fe8e7f5fd77ad2
radgra <rgrabos@inwerken.de>
2019-07-29 07:38:06 +0200

    added players data

--- src/app/models/player.model.ts
+++ src/app/models/player.model.ts
@@ -1,7 +1,19 @@
 import { Team } from './team.model';
 export interface Player {
+    id?:number
     firstName?:string
     lastName:string;
     born?:Date
     team?:Team
+}
+
+
+// M2M worldcup/player
+type Position = "Bramkarz" | "Obronca" | "Pomocnik" | "Napastnik"; 
+export interface Squad {
+    player:Player
+    year:number
+    appearances:number
+    goals:number;
+    position: Position
 }
